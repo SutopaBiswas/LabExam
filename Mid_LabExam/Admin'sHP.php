60f2f5479d4cf081b5c1f795fe8b36fd9a06128a
SutopaBiswas <60354707+SutopaBiswas@users.noreply.github.com>
2020-02-26 16:56:39 +0600

    Add files via upload

---
+++ Mid_LabExam/Admin'sHP.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<center>
+		<h1>Welcome Bob!</h1>
+	</center>
+</head>
+<body>
+	<center>
+		<a href="Profile.php">Profile</a></br>
+		<a href="ChangePassword.php">Change Password</a></br>
+		<a href="ViewUsers.php">View Users</a></br>
+		<a href="Login.php">Logout</a></br>
+	</center>
+	
+</body>
+</html>
---
+++ Mid_LabExam/ChangePassword.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+	<form method="POST" action="ChangePassword.php" >
+	<fieldset>
+		<legend>CHANGE PASSWORD</legend>
+	    Current Password</br><input type="password" name="password" value=""/></br>
+		New Password</br><input type="password" name="password" value=""/></br>
+		Retype New Password</br><input type="password" name="password" value=""/></br>
+		<input type="submit" name="submit" value="Change"> 
+		<a href="Home.php">Home</a>
+	</fieldset>
+	</form>
+</body>
+
+
+</html>
